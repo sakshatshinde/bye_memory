5df7cf7b5009b4c2adc914942355193c34968d05
Sakshat Shinde <shindesakshat@gmail.com>
2020-04-27 11:17:28 +0530

    Fix for Windows
    Allocation speed reduced greatly to make it more stealthy, and it does not suddenly spike anymore

--- main.c
+++ main.c
@@ -5,47 +5,62 @@
 #ifdef _WIN32
 
 #include <windows.h>
-//-----
-unsigned long long getTotalSystemMemory()
-{
-    MEMORYSTATUSEX status;
-    status.dwLength = sizeof(status);
-    GlobalMemoryStatusEx(&status);
-    return status.ullTotalPhys;
-}
-//-----
+        //-----
+        unsigned long long getTotalSystemMemory(){
+            MEMORYSTATUSEX status;
+            status.dwLength = sizeof(status);
+            GlobalMemoryStatusEx(&status);
+            return status.ullTotalPhys;
+        }
+        //-----
+        
+        unsigned long long getUsedSystemMemory(){
+            MEMORYSTATUSEX memInfo;
+			memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+			GlobalMemoryStatusEx(&memInfo);
+			DWORDLONG physMemUsed = memInfo.ullTotalPhys - memInfo.ullAvailPhys;
+			return physMemUsed;
+        }
+        
+		
 #define TOTAL_MEMORY getTotalSystemMemory()
+#define USED_MEMORY getUsedSystemMemory()
 
 #endif
 
 #ifdef __linux__
 
 #include <unistd.h>
-//-----
-unsigned long long getTotalSystemMemory()
-{
-    long pages = sysconf(_SC_PHYS_PAGES);
-    long page_size = sysconf(_SC_PAGE_SIZE);
-    return pages * page_size;
-}
-//-----
+        //-----
+        unsigned long long getTotalSystemMemory(){
+            long pages = sysconf(_SC_PHYS_PAGES);
+            long page_size = sysconf(_SC_PAGE_SIZE);
+            return pages * page_size;
+        }
+        //-----
 #define TOTAL_MEMORY getTotalSystemMemory()
 
 #endif
 
 int main(int argc, char** argv){
 
-
 //long long -> ll and unsigned -> u
-printf("%llu\n", TOTAL_MEMORY);
+printf("This is the Total Memory: %llu\n", TOTAL_MEMORY);
 long long* flood = NULL;
-unsigned long long LIMIT =  ((unsigned long long) TOTAL_MEMORY * 0.3);
+float flood_percentage;
 
-int i = 0;
-while(i < (TOTAL_MEMORY * 0.3)){
+printf("Input the flood percentage :");
+scanf("%f", &flood_percentage);
+unsigned long long LIMIT =  ((unsigned long long) TOTAL_MEMORY * (flood_percentage / 100));
+printf("Current limit : %llu\n", LIMIT);
+printf("Used memory : %llu", USED_MEMORY);
+
+
+// int i = 0;
+while(USED_MEMORY < LIMIT){	// This while condition is meaningless atm, always true
     //printf("Total memory: %llu", TOTAL_MEMORY);
     flood = malloc(sizeof(flood));
-    i++;
+//    i = i + sizeof(flood);
     // sleep(1);
 }
 
@@ -55,5 +70,6 @@ if (flood == NULL) {
 }
 
 free(flood);
+printf("Flooding Done!");
 return 0;
 }
